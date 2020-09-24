345396ed7f0c46c14c4ceacab997d2f2adc55b96
Gentur <guntutur@gmail.com>
2020-09-24 14:38:09 +0700

    style(init): add some style to list task
    Using some animation from react-flip-move.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import TodoList from "./TodoList";
+
+var destination = document.querySelector("#container");
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <div>
+        <TodoList/>
+    </div>,
+    destination
 );
 
 // If you want your app to work offline and load faster, you can change
